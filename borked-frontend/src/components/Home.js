c69c1953a2cd95db0b305a6810b93c19bbfa55ec
Irtaza2009 <irtazanaqvi05@gmail.com>
2026-01-08 22:06:40 +0500

    added description

--- borked-frontend/src/components/Home.js
+++ borked-frontend/src/components/Home.js
@@ -14,13 +14,13 @@ const Home = ({ user }) => {
       </h2>
 
       <div className="home-text" style={{ display: "flex", flexDirection: "column", gap: "0.75rem", alignItems: "center" }}>
-        {/** <h3 style={{ margin: 0, letterSpacing: "0.12em", textTransform: "uppercase" }}>A Bad UI Competition</h3> **/}
+        <h3 style={{ margin: 0, letterSpacing: "0.12em", textTransform: "uppercase" }}>A Bad UI Competition</h3> 
         {!user && (
           <button type="button" onClick={login}>Login with Slack</button>
         )}
       </div>
 
-      {/**  <p className="home-text">A <b><i>bad</i> UI competition</b>! The <del>best</del> <b>worst</b> project gets a <a href="https://en.wikipedia.org/wiki/Useless_machine">Useless Box</a>!</p> **/}
+      <p className="home-text">A <b><i>bad</i> UI competition</b>! The <del>best</del> <b>worst</b> project gets a <a href="https://en.wikipedia.org/wiki/Useless_machine">Useless Box</a>!</p> 
       <p className="home-text">more info goes here blah blah blah...</p>
 
       <HourglassLoader />
